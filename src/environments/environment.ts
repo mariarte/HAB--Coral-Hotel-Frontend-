1fe74805b572e1b5edb40cde30ade59e406a49f4
mariarte <maria.arjim@gmail.com>
2019-06-18 16:38:29 +0200

    [ADD: Deploy]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: "http://localhost:3000/api"
-  // apiBaseUrl: "https://git.heroku.com/coralhotel.git"
+  // apiBaseUrl: "https://coralhotel.herokuapp.com/api"
 };
 
 /*
